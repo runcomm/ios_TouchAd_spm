ece5450f541fb073698e5b91e654ad228eaf14a6
iyunpyo <poqwer233@naver.com>
2023-08-08 20:54:43 +0900

    20230808 BC 페이북 iOS Touchad SPM 1. package 코드에 target 코드 주석 해제

--- Package.swift
+++ Package.swift
@@ -19,16 +19,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "TouchadSDK",
-//            dependencies: ["Alamofire"]
-//        ),
+        .target(
+            name: "TouchadSDK",
+            dependencies: ["Alamofire"]
+        ),
 //        .testTarget(
 //            name: "TouchadSDKTests",
 //            dependencies: ["TouchadSDK","Alamofire"]
 //        ),
         .binaryTarget(
-            name: "TouchadSDK",
+            name: "TouchadSDK.xcframework",
             path: "TouchadSDK.xcframework")
     ]
 )

35cf4c2b9b3878556ba285a85ff861ee8416a1d7
iyunpyo <poqwer233@naver.com>
2023-08-08 14:40:51 +0900

    20230808 BC 페이북 Touchad SPM 1. xcframework를 이용한 SPM 셋팅 검토 중

--- Package.swift
+++ Package.swift
@@ -14,18 +14,29 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "4.9.1"),
+        //.package(url: "https://github.com/Alamofire/Alamofire.git", exact: "4.9.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "TouchadSDK",
+//            dependencies: ["Alamofire"]
+//        ),
+//        .testTarget(
+//            name: "TouchadSDKTests",
+//            dependencies: ["TouchadSDK","Alamofire"]
+//        ),
         .target(
             name: "TouchadSDK",
-            dependencies: ["Alamofire"]
+            dependencies: []
         ),
         .testTarget(
             name: "TouchadSDKTests",
-            dependencies: ["TouchadSDK","Alamofire"]
+            dependencies: ["TouchadSDK"]
         ),
+        .binaryTarget(
+            name: "TouchadSDK.xcframework",
+            path: "TouchadSDK.xcframework")
     ]
 )

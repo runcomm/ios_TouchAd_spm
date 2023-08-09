d6d56731c19f78b064b831c889856e68e2a90756
iyunpyo <poqwer233@naver.com>
2023-08-09 17:20:40 +0900

    20230809 BC 페이북 iOS Touchad SPM 1. Alamofire dependency 주석 해제 2. 로컬로 적용한 Alamofire.xcframework 삭제 3. Sources 폴더 내 파일 삭제

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TouchadSDK",
-            targets: ["TouchadSDK", "Alamofire"]),
+            targets: ["TouchadSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        //.package(url: "https://github.com/Alamofire/Alamofire.git", exact: "4.9.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "4.9.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,9 +30,6 @@ let package = Package(
         .binaryTarget(
             name: "TouchadSDK",
             path: "TouchadSDK.xcframework"
-        ),
-        .binaryTarget(
-            name: "Alamofire",
-            path: "Alamofire.framework")
+        )
     ]
 )
